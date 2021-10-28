5f29413cc0fcaaa5be601c665ffb1e3a6d5a4d4d
WahidinAji <a17wahidin@gmail.com>
2021-10-28 10:35:34 +0700

    Simple convert of multiple of twenty five

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+function multipleOfTwentyFive($input)
+{
+    $round = intval($input);
+    $get_after_comma = $input - $round;
+    $result = 0;
+    switch ($get_after_comma) {
+        case $get_after_comma == 0.00 && $get_after_comma >= 0.76:
+            $result = number_format($round, 2);
+            break;
+        case $get_after_comma > 0.00 && $get_after_comma <= 0.25:
+            $result = number_format($round + 0.25, 2);
+            break;
+        case $get_after_comma > 0.25 && $get_after_comma <= 0.50:
+            $result = number_format($round + 0.50, 2);
+            break;
+        case $get_after_comma > 0.50 && $get_after_comma <= 0.75:
+            $result = number_format($round + 0.75, 2);
+            break;
+        default:
+            $result = number_format($round, 2);
+            break;
+    }
+    return $result;
+}
+
+$a = multipleOfTwentyFive(25.001);
+echo $a;
+//output    =   25.25
